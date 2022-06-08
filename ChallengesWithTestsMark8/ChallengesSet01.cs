efa9ac3e2a1ae74d236dc3933b789daaa4787cfb
Katelind Phillips <scarlettkate94@gmail.com>
2022-06-08 17:14:41 -0400

    week3 uploads

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -18,32 +18,54 @@ namespace ChallengesWithTestsMark8
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var answer = minuend -= subtrahend;
+            return answer;
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var answer = number1 += number2;
+            return answer;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            var answer = factor1 *= factor2;
+            return answer;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return $"Hello!";
+            }
+            else return $"Hello, {nameOfPerson}!";
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            return "HEY!";
+
         }
     }
 }
